62eac245971559a0f8874fa898764bdfb03c1fb3
daniel.ducuara <daniel.ducuara@outscale.com>
2024-08-04 19:21:21 +0000

    Fixed wireguard init

--- deploy/resources/init_vm_public.sh
+++ deploy/resources/init_vm_public.sh
@@ -8,5 +8,7 @@ sudo systemctl start docker
 sudo systemctl status docker
 sudo usermod -aG docker $(whoami)
 sudo chmod 666 /var/run/docker.sock
+sudo modprobe ip_tables
+sudo echo 'ip_tables' >> sudo /etc/modules
 git clone https://github.com/DanielDucuara2018/video_streaming.git /opt/video_streaming
 docker compose -f /opt/video_streaming/deploy/resources/docker-compose.yml up -d
